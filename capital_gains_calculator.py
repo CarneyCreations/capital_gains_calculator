fd49bcbbcbe108d2305437ed50ce55bf2bd08054
“Joshua <joshcarney@hotmail.co.uk>
2023-01-30 21:08:00 +0000

    Added menu option to dictate trading rule required > > Removed all commas from within each trade (which separates large numbers) and the coin names that trail numbers. The trades are now ready for calculating. > > Added a menu option to process trades with a cost-basis, and allocates them depending on which financial rules they fall under. Rules are decided on when the trade was made and this code covers 2/3 of those rules – day-trade and bed and breakfast. The files will be: <coin_name> + day_trade or <coin>bed_and_breakfast.

--- capital_gains_calculator.py
+++ capital_gains_calculator.py
@@ -1,56 +1,117 @@
 import csv
-import tabulate
+# import tabulate
 
 
-def separate_coins(lead_coin):
+def calculate_cost_basis(cost, amount, fee):
+    """ Calculates the cost basis of coins."""
+    cost_basis = (float(cost) * float(amount)) + float(fee)
+    print(f"({cost} * {amount}) + {fee} = {cost_basis}")
+    return cost_basis
+
+
+def separate_coins(trade):
     """ Organises each trade in a specific coin and a GBP pair to its own text file. """
-    for check_column in csv_data:
-        if lead_coin == check_column[1] and "GBP" in check_column[2]:
-            lead_coin = open(lead_coin, "a+")           # End game for writing files.
-            # lead_coin = open(lead_coin, "w+")         # To clear files.
-            lead_coin.write(str(column) + "\n")
+    lead_coin = trade[1]
+    buy_or_sell = trade[3]
+    cost = trade[4]
+    amount_executed = trade[5]
+    fee = trade[7]
 
+    if buy_or_sell == "BUY":
+        pass
+        cost_basis = calculate_cost_basis(cost, amount_executed, fee)
+        trade[8] = cost_basis
+
+    elif buy_or_sell == "SELL":
+        pass
+        print("Sell Side")
+
+
+    lead_coin = open(lead_coin, "a+")           # End game for writing files.
+    # lead_coin = open(lead_coin, "w+")         # To clear files.
+    lead_coin.write(str(trade) + "\n")
     lead_coin.close()
 
 
-double_crypto_pair = []
+def alternative_pair(no_cost_basis):
+    """ Uploads any trades without a cost basis to a separate file where one will be created. """
+    # crypto_crypto = open("alternative_pairs.txt", "w+")
+    trades_to_be_processed = open("alternative_pairs.txt", "a+")
+    trades_to_be_processed.write(str(no_cost_basis) + "\n")
+    no_cost_basis_list.append(no_cost_basis)
+    no_cost_basis_list.append("\n")
+
+
+
+no_cost_basis_list = []
 csv_data = []
 
-# encoding="ISO-8859-1" prevents the csv error:
-# UnicodeDecodeError: 'utf-8' codec can't decode byte 0xff in position 0: invalid start byte
-# binance_csv = open("/Users/josh/Git/capital_gains_calculator/Binance/2020-2021 Tax Year (complete) (SPLIT PAIRS).csv",
-#                    "r", encoding="ISO-8859-1")
-binance_csv = open("test_csv.csv",
-                   "r", encoding="ISO-8859-1")
-
-# Converts from a TextIOWrapper to a csv.reader object.
-csvreader = csv.reader(binance_csv)
-
-# Adds the first row (header) to "csv_header" and iterates to the second row.
-csv_header = next(csvreader)
-
-# Converts from a csv.reader object in to a list.
-for row in csvreader:
-    csv_data.append(row)
-binance_csv.close()
-
-# for column in csvreader:
-for column in csv_data:
-    crypto = column[1]
-    pair = column[2]
-
-    if "GBP" in pair:                       # If already calculable.
-        separate_coins(crypto)
-
-    # If the pair isn't GBP it is added to a separate text file, ready for me to check th fair market value.
-    elif "GBP" not in pair:
-        crypto_crypto = open("crypto_crypto_pair.txt", "a+")
-        crypto_crypto.write(str(column) + "\n")
-        double_crypto_pair.append(column)
-        double_crypto_pair.append("\n")
 
 
-print(double_crypto_pair)       # Trades which are written to the alternate-pairs file.
+while True:
+    menu_selection = input("\n\n"
+                           "1. Read and process CSV file.\n"
+                           "2. Upload information to new files.\n"
+                           "0. Exit program.\n"
+                           ">>> ")
+
+
+    # Read and process CSV file.
+    if menu_selection == "1":
+        # TODO: Display csv files in the directory folder and add an input that dictates which .csv to read.
+        # encoding="ISO-8859-1" prevents the csv error:
+        # UnicodeDecodeError: 'utf-8' codec can't decode byte 0xff in position 0: invalid start byte
+        # binance_csv = open("/Users/josh/Git/capital_gains_calculator/binance/2020_2021_binance_split_pairs(complete).csv",
+        #                    "r", encoding="ISO-8859-1")
+        binance_csv = open("test_csv.csv",
+                           "r", encoding="ISO-8859-1")
+
+        # Converts from a TextIOWrapper to a csv.reader object.
+        csvreader = csv.reader(binance_csv)
+
+        # Adds the first row (header) to "csv_header" and iterates to the second row.
+        csv_header = next(csvreader)
+
+
+        # Converts from a csv.reader object in to a list.
+        for each_trade in csvreader:
+            for index, word in enumerate(each_trade[5:8]):        # For the fields with coin prices, fees or quantities.
+                without_coin_name = ""                            # Resets value to avoid data redundancy.
+
+                for character in word:
+                    with_coin_name = character.strip(",ABCDEFGHIJKLMNOPQRSTUVWXYZ")      # Removes coin name and thousands seperator.
+                    without_coin_name += with_coin_name
+                    each_trade[index+5] = without_coin_name         # Reassigns the value with commas or letters.
+
+            csv_data.append(each_trade)
+        binance_csv.close()
+
+
+    # Upload information to new files.
+    elif menu_selection == "2":
+        # for column in csvreader:
+
+        # Needed to pass the crypto into the function.
+        for column in csv_data:
+            # crypto = column[1]
+            # pair = column[2]
+            # column = column.remove(",")
+
+            if "GBP" in column[2]:                       # If already calculable.
+                separate_coins(column)
+
+            # If the pair isn't GBP/has no cost basis, it is added to a separate text file, ready for me to check
+            # and assign the fair market value.
+            elif "GBP" not in column[2]:
+                alternative_pair(column)
+
+        # Trades which are written to the alternate-pairs file.
+        print(f"These are the trades which do not have a cost basis yet: \n{no_cost_basis_list}")
+
+
+    # Exit program.
+    elif menu_selection == "0":
+        break
 
 """
 So far the program:
